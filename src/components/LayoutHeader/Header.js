502075ba639b7fee2647da18e31fd7ac8c445e2b
Oskari Liukku <oliukku@gmail.com>
2018-09-24 16:39:19 +0300

    New favicons, new logo

--- src/components/LayoutHeader/Header.js
+++ src/components/LayoutHeader/Header.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import Headroom from 'react-headroom';
+import React from "react";
+import Headroom from "react-headroom";
 
-import DropdownMenu from './DropdownMenu';
-import DesktopMenu from './DesktopMenu';
+import DropdownMenu from "./DropdownMenu";
+import DesktopMenu from "./DesktopMenu";
 
-import logoFull from '../../assets/icons/improxLogoWhite_small.png';
-import {colors} from '../../theme';
-import './Header.css';
+import logoFull from "../../assets/icons/improxLogo.png";
+import { colors } from "../../theme";
+import "./Header.css";
 
 const Header = () => (
-  <Headroom
-    disable
-    style={{
-      width: '100%',
-    }}
-  >
-    <div className="header-container">
-      <a
-        href="/"
+    <Headroom
+        disable
         style={{
-          color: colors.light,
+            width: "100%",
         }}
-        className="logo-container"
-      >
-        <img src={logoFull} alt="Improx Games" className="logo" />
-      </a>
-      <DropdownMenu />
-      <DesktopMenu />
-    </div>
-  </Headroom>
+    >
+        <div className="header-container">
+            <a
+                href="/"
+                style={{
+                    color: colors.light,
+                }}
+                className="logo-container"
+            >
+                <img src={logoFull} alt="Improx Games" className="logo" />
+            </a>
+            <DropdownMenu />
+            <DesktopMenu />
+        </div>
+    </Headroom>
 );
 
 export default Header;

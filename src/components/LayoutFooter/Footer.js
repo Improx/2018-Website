72d55906964f5f8ea7526d05427b9fe72490762a
Oskari Liukku <oliukku@gmail.com>
2020-01-04 19:38:19 +0200

    Tweak footer

--- src/components/LayoutFooter/Footer.js
+++ src/components/LayoutFooter/Footer.js
@@ -1,65 +1,71 @@
-import React from 'react';
+import React from "react";
 
-import {css} from 'glamor';
-import Scroll from 'react-scroll';
-import FaCopyright from 'react-icons/lib/fa/copyright';
+import { css } from "glamor";
+import Scroll from "react-scroll";
+import FaCopyright from "react-icons/lib/fa/copyright";
 
-import {fonts, colors} from '../../theme';
-import FooterRow from './FooterRow';
-import ExternalIconLink from './ExternalIconLink';
+import { fonts, colors } from "../../theme";
+import FooterRow from "./FooterRow";
+import ExternalIconLink from "./ExternalIconLink";
 
 const socialInfo = [
   {
-    path: 'https://twitter.com/improxgames',
-    icon: 'twitter',
+    path: "https://twitter.com/improxgames",
+    icon: "twitter"
   },
   {
-    path: 'https://facebook.com/improxgames',
-    icon: 'facebook',
+    path: "https://facebook.com/improxgames",
+    icon: "facebook"
   },
   {
-    path: 'https://instagram.com/improxgames',
-    icon: 'instagram',
+    path: "https://instagram.com/improxgames",
+    icon: "instagram"
   },
   {
-    path: 'mailto:contact@improxgames.com',
-    icon: 'email',
-  },
+    path: "mailto:contact@improxgames.com",
+    icon: "email"
+  }
 ];
 
 const footerStyle = css({
   backgroundColor: colors.black,
   color: colors.light,
-  paddingTop: 30,
+  paddingTop: 10,
   paddingBottom: 30,
-  fontFamily: fonts.primary,
+  fontFamily: fonts.primary
 });
 
 const footerHeaderStyle = css({
-  textAlign: 'center',
+  textAlign: "center",
   margin: 0,
-  marginBottom: '0.3em',
-  marginTop: '1em',
+  marginBottom: "0.3em",
+  marginTop: "1em"
 });
 
 const Footer = () => (
   <Scroll.Element name="contact">
     <div>
       <footer {...footerStyle}>
-        <hr {...css({color: colors.yellow})} />
         <h3 {...footerHeaderStyle}>Contact us at:</h3>
         <FooterRow>
           {socialInfo.map((x, i) => {
-            return <ExternalIconLink href={x.path} alt={'Improx Games ' + x.icon} icon={x.icon} key={i} />;
+            return (
+              <ExternalIconLink
+                href={x.path}
+                alt={"Improx Games " + x.icon}
+                icon={x.icon}
+                key={i}
+              />
+            );
           })}
         </FooterRow>
         <br />
         <FooterRow>
-          <div {...css({color: colors.grey, fontFamily: fonts.secondary})}>
+          <div {...css({ color: colors.grey, fontFamily: fonts.secondary })}>
             <FaCopyright
               color={colors.grey}
-              size={'20'}
-              viewBox={'0 0 45 45'}
+              size={"20"}
+              viewBox={"0 0 45 45"}
             />
             Improx Games 2018
           </div>
--- src/components/LayoutMission/BackgroundVideo.js
+++ src/components/LayoutMission/BackgroundVideo.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import {css} from 'glamor';
+import React from "react";
+import { css } from "glamor";
 
-import {colors} from '../../theme';
+import { colors } from "../../theme";
 
-import video from '../../assets/videos/improx_loop.mp4';
-import poster from '../../assets/videos/poster.webp';
+import video from "../../assets/videos/improx_loop.mp4";
+import poster from "../../assets/videos/poster.webp";
 
 let rule = css({
-  position: 'absolute',
+  position: "absolute",
   right: 0,
   bottom: 0,
-  minWidth: '100%',
-  minHeight: '100%',
-  width: 'auto',
-  height: 'auto',
+  minWidth: "100%",
+  minHeight: "100%",
+  width: "auto",
+  height: "auto",
   zIndex: -100,
-  filter: 'brightness(30%)',
-  backgroundColor: colors.black,
+  filter: "brightness(30%)",
+  backgroundColor: colors.black
 });
 
 const BackgroundVideo = () => (
